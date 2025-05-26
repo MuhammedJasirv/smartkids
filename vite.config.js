f530b5a45ceefb8ae4c0fd3a03b7628186681cf6
MuhammedJasirv <muhammed.jasir@ivorytechnologies.com>
2025-05-26 10:53:46 +0530

    Router Issue fixing

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-build: {
+  build: {
     outDir: 'dist'
   },
-  server: {
-    historyApiFallback: true // needed for react-router-dom
-  }
-})
+  base: '/',
+});
