406f60f8ce917680aeaff8f00b63ca5e60058d87
MuhammedJasirv <muhammed.jasir@ivorytechnologies.com>
2025-05-31 00:06:03 +0530

    add Responsve

--- src/components/home/howitwork/Howitwork.jsx
+++ src/components/home/howitwork/Howitwork.jsx
@@ -84,7 +84,7 @@ const Howitwork = () => {
       {/* Hand image with animation */}
       <motion.img
         src={Hand}
-        className="absolute -top-8 right-[7%] z-20"
+        className="absolute -top-8 right-[7%] z-20 lg:flex hidden"
         alt=""
         initial={{ x: 100, opacity: 0 }}
         animate={headerInView ? { x: 0, opacity: 1 } : {}}
@@ -94,7 +94,7 @@ const Howitwork = () => {
       {/* Header section */}
       <motion.div
         ref={headerRef}
-        className="w-full p-14 pl-32"
+        className="w-full lg:p-14 lg:pl-32 text-center lg:text-start"
         initial="hidden"
         animate={headerInView ? "visible" : "hidden"}
         variants={containerVariants}
@@ -106,7 +106,7 @@ const Howitwork = () => {
           How does it work?
         </motion.p>
         <motion.p
-          className="text-white font-sans font-normal text-sm xs:text-base sm:text-lg md:text-xl lg:text-lg xl:text-lg w-full xs:w-4/5 sm:w-3/4 md:w-2/3 lg:w-1/2 mt-2 xs:mt-3 sm:mt-4 pr-2"
+          className="text-white font-sans font-normal text-sm xs:text-base sm:text-lg md:text-xl lg:text-lg xl:text-lg w-full   mx-auto lg:mx-0 md:w-2/3 lg:w-1/2 mt-2 xs:mt-3 sm:mt-4 pr-2"
           variants={textVariants}
         >
           To understand the working of the pocket money solution it's important
@@ -122,41 +122,53 @@ const Howitwork = () => {
         animate={parentSectionInView ? "visible" : "hidden"}
         variants={containerVariants}
       >
-        <div className="relative w-full lg:w-1/2 flex justify-center lg:justify-end lg:pr-20 mb-8 lg:mb-0 ">
-          <motion.div
-            className="bg-gradient-to-b from-[#3E89C8] to-[#95C83E] w-32 xs:w-36 sm:w-40 md:w-44 lg:w-48 xl:w-52 2xl:w-56 h-[250px] xs:h-[280px] sm:h-[320px] md:h-[350px] lg:h-[350px] absolute rounded-4xl -top-3 z-0 right-4 xs:right-6 sm:right-10 md:right-12 lg:right-16"
-            variants={imageVariants}
-          />
-
-          <motion.div
-            className="absolute z-20 -right-8 xs:-right-10 sm:-right-16 md:-right-24 lg:-right-0 xl:-right-32 flex justify-center items-center"
-            variants={cardVariants}
-            whileHover={{ y: -5 }}
-          >
-            <img
-              src={IdCard}
-              alt=""
-              className="w-24 xs:w-28 sm:w-32 md:w-36 lg:w-40 xl:w-auto xl:max-w-[342px] hidden"
+        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end lg:pr-20 mb-8 lg:mb-0 ">
+          <div className="relative p-3 mt-6 lg:mt-0 lg:p-0">
+            {/* Gradient background */}
+            <motion.div
+              className="bg-gradient-to-b from-[#3E89C8] to-[#95C83E] 
+        w-32 xs:w-36 sm:w-40 md:w-44 lg:w-48 xl:w-52 2xl:w-56 
+        h-[180px] xs:h-[210px] sm:h-[250px] md:h-[280px] lg:h-[300px] 
+        absolute rounded-4xl -top-0 xs:top-0 md:-top-3 right-0 xs:right-0 sm:right-0  md:-right-3  lg:-right-6 
+        z-0"
+              variants={imageVariants}
             />
-          </motion.div>
 
-          <motion.div
-            className="absolute left-[10%] xs:left-[12%] sm:left-[15%] md:left-[18%] lg:left-[22%] z-20 top-20 xs:top-24 sm:top-28 md:top-32 lg:top-36"
-            variants={imageVariants}
-          >
-            <img
-              src={Histroy}
+            {/* Main image (on top) */}
+            <motion.img
+              src={ImgOne}
               alt=""
-              className="w-20 xs:w-24 sm:w-28 md:w-32 lg:w-36 xl:w-auto"
+              className="relative z-10 w-40 xs:w-48 sm:w-56 md:w-64 lg:w-72 xl:w-auto"
+              variants={imageVariants}
             />
-          </motion.div>
 
-          <motion.img
-            src={ImgOne}
-            className="z-10 w-40 xs:w-48 sm:w-56 md:w-64 lg:w-72 xl:w-auto"
-            alt=""
-            variants={imageVariants}
-          />
+            {/* IdCard overlay (above gradient but behind ImgOne) */}
+            <motion.div
+              className="absolute z-20 -right-9 xs:top-4 sm:top-0 xs:-right-10 sm:-right-16 top-5 md:-right-24 lg:-right-20 xl:-right-48 
+        flex justify-center items-center"
+              variants={cardVariants}
+              whileHover={{ y: -5 }}
+            >
+              <img
+                src={IdCard}
+                alt=""
+                className="w-28 xs:w-32 sm:w-40 md:w-48 lg:w-52 xl:w-auto xl:max-w-[342px]"
+              />
+            </motion.div>
+
+            {/* History icon (in between) */}
+            <motion.div
+              className="absolute z-20 -left-[30%] xs:-left-[40%] sm:-left-[40%] md:-left-[30%] lg:-left-[52%] 
+        top-14  xs:top-18 sm:top-20 md:top-32 lg:top-36"
+              variants={imageVariants}
+            >
+              <img
+                src={Histroy}
+                alt=""
+                className="w-28  xs:w-32 sm:w-38 md:w-42 lg:w-52 xl:w-auto"
+              />
+            </motion.div>
+          </div>
         </div>
 
         <motion.div
@@ -202,37 +214,52 @@ const Howitwork = () => {
           </p>
         </motion.div>
 
-        <div className="relative w-full lg:w-[30%] flex justify-center lg:pr-20 order-1 lg:order-2">
-          <motion.div
-            className="bg-gradient-to-b from-[#3E89C8] to-[#95C83E] w-32 xs:w-36 sm:w-40 md:w-44 lg:w-48 xl:w-52 h-[250px] xs:h-[280px] sm:h-[320px] md:h-[350px] lg:h-[350px] absolute rounded-4xl -ml-20 xs:-ml-24 sm:-ml-28 md:-ml-32 lg:-ml-40 -top-4 z-0"
-            variants={imageVariants}
-          />
+        <div className="w-full lg:w-[30%] flex justify-center lg:pr-20 order-1 lg:order-2">
+  <div className="relative p-3 mt-6 lg:mt-0 lg:p-0">
+    
+    {/* Gradient Background */}
+    <motion.div
+      className="bg-gradient-to-b from-[#3E89C8] to-[#95C83E] 
+                 w-32 xs:w-36 sm:w-40 md:w-44 lg:w-48 xl:w-52 2xl:w-56 
+                h-[180px] xs:h-[210px] sm:h-[240px] md:h-[270px] lg:h-[250px] xl:h-[340px] 2xl:h-[340px] 
+                 absolute rounded-4xl -top-0 xs:top-0 md:-top-0 left-0 xs:left-0 sm:left-0  md:-left-2  lg:-left-6 z-0"
+      variants={imageVariants}
+    />
 
-          <motion.img
-            src={ImgTwo}
-            className="z-10 w-40 xs:w-48 sm:w-56 md:w-64 lg:w-72 xl:w-80 max-w-full"
-            alt=""
-            variants={imageVariants}
-          />
+    {/* Main Image */}
+    <motion.img
+      src={ImgTwo}
+      className="relative z-10 w-40 xs:w-48 sm:w-56 md:w-64 lg:w-72 xl:w-80 max-w-full"
+      alt=""
+      variants={imageVariants}
+    />
 
-          <motion.img
-            src={Transaction}
-            className="absolute z-20 -left-8 xs:-left-10 sm:-left-12 md:-left-16 lg:-left-20 xl:-left-36 -top-3 w-16 xs:w-20 sm:w-24 md:w-28 lg:w-32 xl:w-auto"
-            alt=""
-            initial={{ y: -20, opacity: 0 }}
-            animate={childSectionInView ? { y: 0, opacity: 1 } : {}}
-            transition={{ delay: 0.3 }}
-          />
+    {/* Transaction Icon */}
+    <motion.img
+      src={Transaction}
+      className="absolute z-20 -left-14 xs:-left-10 sm:-left-12 md:-left-10 lg:-left-20 xl:-left-48 
+                 top-2 xs:top-4 md:top-3 sm:top-4 lg:-top-3 w-32 xs:w-28 sm:w-36 md:w-36 lg:w-44 xl:w-auto"
+      alt=""
+      initial={{ y: -20, opacity: 0 }}
+      animate={childSectionInView ? { y: 0, opacity: 1 } : {}}
+      transition={{ delay: 0.3 }}
+    />
 
-          <motion.img
-            src={TransactionSilver}
-            className="absolute z-20 -bottom-6 xs:-bottom-8 sm:-bottom-10 md:-bottom-12 right-0 w-16 xs:w-20 sm:w-24 md:w-28 lg:w-32 xl:w-auto"
-            alt=""
-            initial={{ y: 20, opacity: 0 }}
-            animate={childSectionInView ? { y: 0, opacity: 1 } : {}}
-            transition={{ delay: 0.4 }}
-          />
-        </div>
+    {/* Transaction Silver Icon */}
+    <motion.img
+      src={TransactionSilver}
+      className="absolute z-20 bottom-0 xs:-bottom-0 sm:-bottom-0 md:-bottom-0  lg:bottom-0 
+
+                right-3 xs:right-0 sm:right-0 md:right-0 lg:-right-0 xl:-right-30 w-32 xs:w-28 sm:w-36 md:w-36 lg:w-44 xl:w-auto"
+      alt=""
+      initial={{ y: 20, opacity: 0 }}
+      animate={childSectionInView ? { y: 0, opacity: 1 } : {}}
+      transition={{ delay: 0.4 }}
+    />
+
+  </div>
+</div>
+
       </motion.div>
     </div>
   );
