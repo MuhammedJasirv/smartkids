3c69eb2d46f0d9b1149c14785fe0a61d235fe6e3
MuhammedJasirv <muhammed.jasir@ivorytechnologies.com>
2025-06-16 21:03:49 +0530

    complete the parentsPage

--- src/pages/Parents.jsx
+++ src/pages/Parents.jsx
@@ -4,6 +4,9 @@ import Image from "../assets/images/about/navigation/Bg.png";
 import Heder from "../components/parents/heder/Heder";
 import About from "../components/parents/about/About";
 import Features from "../components/parents/features/Features";
+import Service from "../components/parents/service/Service";
+import FooterBanner from "../components/common/footerBanner/FooterBanner";
+import Footer from "../components/common/footer/Footer";
 
 const Parents = () => {
   return (
@@ -20,6 +23,17 @@ const Parents = () => {
       </div>
       <div>
         <Features/>
+      </div>
+      <div>
+        <Service/>
+      </div>
+      <div className="relative h-24">
+        <div className="w-full md:absolute mt-7">
+        <FooterBanner color={"8CBE3E"} width={"80%"} />
+        </div>
+      </div>
+      <div>
+        <Footer Colour="#001830" />
       </div>
     </div>
   );
