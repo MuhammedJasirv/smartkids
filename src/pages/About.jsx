cbd4023d1349132b193f3ee5473da881502e0f34
MuhammedJasirv <muhammed.jasir@ivorytechnologies.com>
2025-06-17 13:13:52 +0530

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navigation from '../components/common/navigation/Navigation'
 import Image from '../assets/images/about/navigation/Bg.png'
 import Header from '../components/about/header/Header'
+import AboutOne from '../components/about/about/AboutOne'
 const About = () => {
   return (
     <div>
@@ -10,6 +11,9 @@ const About = () => {
         </div>
         <div >
           <Header/>
+        </div>
+        <div>
+          <AboutOne/>
         </div>
     </div>
   )
